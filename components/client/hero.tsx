7883dd67701b2effceea2f65ab2263e4c7c55562
omar tabbush <omartabbush02@gmail.com>
2024-09-03 18:24:12 +0300

    .

--- components/client/hero.tsx
+++ components/client/hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
             }}
             className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
         >
-            Your project deserves more than just code let's make it extraordinary{", "}
+            Your project deserves more than just code let{"'"}s make it extraordinary{", "}
             <Highlight className="text-black dark:text-white">
                 before it's too late
             </Highlight>.
